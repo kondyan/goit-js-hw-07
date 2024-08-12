ac3e0dfaa040480eee60af8e672534abf3415320
kondyan <arsenij.kondratenko@gmail.com>
2024-08-12 20:17:01 +0200

    2-commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const createLayot = galleryItems
+  .map((e) => {
+    return `<li class="gallery__item">
+          <a class="gallery__link" href="">
+            <img
+              class="gallery__image"
+              src=${e.preview}
+              data-source=${e.original}
+              alt=${e.description}
+            />
+          </a>
+        </li>`;
+  })
+  .join(" ");
+
+gallery.insertAdjacentHTML("afterbegin", createLayot);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.currentTarget === event.target) {
+    return;
+  }
+  const imgLink = event.target.dataset.source;
+  console.log(imgLink);
+});
